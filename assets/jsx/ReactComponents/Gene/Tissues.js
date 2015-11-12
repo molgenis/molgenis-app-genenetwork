8ac7985cfcc8b2a91dff6af403bdc348d6146465
PytrikFolkertsma <juha.m.karjalainen@gmail.com>
2015-11-12 17:35:50 +0100

    Added muscles, bone marrow and skin to the SVG and added hover over effect for both the table rows and the organs in the SVG.

--- assets/jsx/ReactComponents/Gene/Tissues.js
+++ assets/jsx/ReactComponents/Gene/Tissues.js
@@ -5,26 +5,29 @@ var htmlutil = require('../../htmlutil')
 
 var DataTable = React.createClass({
 
+    componentWillMount: function() {
+        var indices = this.props.celltypes.indices
+        var avg = this.props.celltypes.avg
+        this.sortedItems = _.sortBy(this.props.celltypes.header, function(item){
+            return avg[indices[item.name]]
+        }).reverse()
+    },
+
     render: function() {
-        
-    	var indices = this.props.celltypes.indices
-    	var avg = this.props.celltypes.avg
+        var indices = this.props.celltypes.indices
+        var avg = this.props.celltypes.avg
         var p = this.props.celltypes.p
         var stdev = this.props.celltypes.stdev
         
-    	var sortedItems = _.sortBy(this.props.celltypes.header, function(item){
-    	    return avg[indices[item.name]]
-    	}).reverse()
-
-    	var rows = _.map(sortedItems, function(item, i){
-    	    var cls = i % 2 === 0 ? 'datarow evenrow' : 'datarow oddrow';
+    	var rows = _.map(this.sortedItems, function(item, i){
+            var cls = i % 2 === 0 ? 'datarow evenrow' : 'datarow oddrow';
     	    return(
-    		    <tr key={item.name} className = {cls} onMouseOver={this.props.onMouseOver.bind(null, item)}>
+    		    <tr key={item.name} className={cls} onMouseOver={this.props.onMouseOver.bind(null, item)} style={this.props.hoverItem === item.name ? {backgroundColor: 'rgb(255,255,0)'} : {}}>
     		    <td>{item.name}</td>
     		    <td style={{textAlign: 'center'}}>{item.numSamples}</td>
     		    <td style={{textAlign: 'center'}}>{avg[indices[item.name]]}</td>
-                    <td style={{textAlign: 'center'}} dangerouslySetInnerHTML={{__html: htmlutil.pValueToReadable(p[indices[item.name]])}}></td>
-                    <td style={{textAlign: 'center'}} >{stdev[indices[item.name]]}</td>
+                <td style={{textAlign: 'center'}} dangerouslySetInnerHTML={{__html: htmlutil.pValueToReadable(p[indices[item.name]])}}></td>
+                <td style={{textAlign: 'center'}} >{stdev[indices[item.name]]}</td>
     		    </tr>
     	    )
     	}.bind(this))
@@ -40,8 +43,8 @@ var DataTable = React.createClass({
                 <th>SD</th>
                 </tr>
                 {rows}
-            </tbody>
-                </table>
+                </tbody>
+            </table>
         )
     }
 })
@@ -53,22 +56,26 @@ var Tissues = React.createClass({
     },
     
     handleMouseOver: function(item) {
-
-        this.setState({
-            hoverItem: item
-        })
+        if (typeof item === "object"){
+            this.setState({
+                hoverItem: item.name
+            })
+        } else {
+            this.setState({
+                hoverItem: item
+            })
+        }       
     },
     
     render: function(){
-        
-	return (
-		<div className="hflex">
-		<DataTable celltypes={this.props.celltypes} onMouseOver={this.handleMouseOver} />
-                <div style={{width: '20%'}}>
-                <HomoSapiens hoverItem={this.state.hoverItem} />
+    	return (
+    		<div className="hflex">
+        		<DataTable celltypes={this.props.celltypes} hoverItem={this.state.hoverItem} onMouseOver={this.handleMouseOver} />
+                <div style={{width: '21%'}}>
+                <HomoSapiens onMouseOver={this.handleMouseOver} hoverItem={this.state.hoverItem} celltypes={this.props.celltypes} />
                 </div>
-                </div>
-	)
+            </div>
+    	)
     }
 })
 
