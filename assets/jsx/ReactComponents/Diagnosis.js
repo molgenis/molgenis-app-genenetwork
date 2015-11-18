09525be4a400095fc0377ae48a524bbce5f0972d
Tessa Gillett <Tessa@MacBook-Pro-van-Tessa.local>
2015-11-18 15:50:24 +0100

    added phenotype table & changing colours (3 ideas)

--- assets/jsx/ReactComponents/Diagnosis.js
+++ assets/jsx/ReactComponents/Diagnosis.js
@@ -15,20 +15,78 @@ var htmlutil = require('../htmlutil')
 var color = require('../../js/color')
 
 
+
+function HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (arguments.length === 1) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+    };
+}
+
+function getRGB(avg){
+    var v = 1-(parseFloat(avg)/20)
+    var s = 0.6 + (parseFloat(Math.abs(avg))/7)
+    if (parseFloat(avg) >= 0){
+        var colors = HSVtoRGB(0.0389, s, v)
+        return 'rgb(' + colors.r + ',' + colors.g + ', ' + colors.b + ')'
+    } else {
+        var colors = HSVtoRGB(0.58, s, v)
+        return 'rgb(' + colors.r + ',' + colors.g + ', ' + colors.b + ')'
+    }
+}
+
+
+
+
 var ShowPhenotypes = React.createClass({
 
+
     render: function() {
+
+    var hoverZScores = this.props.hoverItem
 
 
         var rows = []
         for (var i = 0; i < this.props.prio.terms.length; i++) {
+
+
+            var zToCol = hoverZScores ? Math.ceil(hoverZScores[i] * 60) : rowtype
+
+            console.log(zToCol)
+
+            var hoverColour =  hoverZScores ? 'rgb(' + zToCol + ',255,255)' : rowtype
+
+            console.log(i + ': ' + hoverColour)
             var rowtype = i % 2 === 0 ? 'datarow evenrow' : 'datarow oddrow'
+            var backgroundColour = hoverZScores ? hoverColour : rowtype
+
+            backgroundColour = hoverZScores ? getRGB(hoverZScores[i]) : rowtype
+
 
             rows.push(
-                <tr className = {rowtype} >
+                <tr style={{backgroundColor: backgroundColour, color: color.colors.gnwhite}} className = {rowtype} >
                 <td style={{textAlign: 'left'}}>{this.props.prio.terms[i].term.name}</td>
                 <td style={{textAlign: 'center'}}>{this.props.prio.terms[i].term.numAnnotatedGenes}</td>
-                <td style={{textAlign: 'center'}}><a style={{color: color.colors.gnblack}} href={this.props.prio.terms[i].term.url} target="_blank">{this.props.prio.terms[i].term.id}</a></td>
+                <td style={{textAlign: 'center'}}><a style={{color: color.colors.gnwhite}} href={this.props.prio.terms[i].term.url} target="_blank">{this.props.prio.terms[i].term.id}</a></td>
+                <td style={{textAlign: 'center'}}>{hoverZScores ? hoverZScores[i] : ""}</td>
                 </tr>
                 )
         }
@@ -40,6 +98,7 @@ var ShowPhenotypes = React.createClass({
             <th style={{textAlign: 'left'}}>PHENOTYPE</th>
             <th style={{textAlign: 'center'}}>ANNOTATED GENES</th>
             <th style={{textAlign: 'center'}}>HPO-TERM</th>
+            <th>Zscore</th>
             
             </tr>
             {rows}
@@ -63,6 +122,153 @@ var ShowPhenotypes = React.createClass({
     }
 })
 
+
+
+
+
+
+var ShowPhenotypes2 = React.createClass({
+
+
+    render: function() {
+
+    var hoverZScores = this.props.hoverItem
+
+
+        var rows = []
+        for (var i = 0; i < this.props.prio.terms.length; i++) {
+
+
+            var zToCol = hoverZScores ? Math.ceil(hoverZScores[i] * 60) : rowtype
+
+            console.log(zToCol)
+
+            var hoverColour =  hoverZScores ? 'rgb(' + zToCol + ',255,255)' : rowtype
+
+            console.log(i + ': ' + hoverColour)
+            var rowtype = i % 2 === 0 ? 'datarow evenrow' : 'datarow oddrow'
+            var backgroundColour = hoverZScores ? hoverColour : rowtype
+
+            backgroundColour = hoverZScores ? getRGB(hoverZScores[i]) : rowtype
+
+
+            rows.push(
+                <tr style={{backgroundColor: backgroundColour}} className = {rowtype} >
+                <td style={{textAlign: 'left'}}>{this.props.prio.terms[i].term.name}</td>
+                <td style={{textAlign: 'center'}}>{this.props.prio.terms[i].term.numAnnotatedGenes}</td>
+                <td style={{textAlign: 'center'}}><a style={{color: color.colors.gnblack}} href={this.props.prio.terms[i].term.url} target="_blank">{this.props.prio.terms[i].term.id}</a></td>
+                <td style={{textAlign: 'center'}}>{hoverZScores ? hoverZScores[i] : ""}</td>
+                </tr>
+                )
+        }
+
+        return (
+            <table className='gn-gene-table datatable' style={{width: '50%'}}>
+            <tbody>
+            <tr>
+            <th style={{textAlign: 'left'}}>PHENOTYPE</th>
+            <th style={{textAlign: 'center'}}>ANNOTATED GENES</th>
+            <th style={{textAlign: 'center'}}>HPO-TERM</th>
+            <th>Zscore</th>
+            
+            </tr>
+            {rows}
+            </tbody>
+            </table>
+            )
+
+        {/*var phenotypes = 'Showing results for: '
+        for (var i = 0; i < this.props.prio.terms.length; i++) {
+            if (i === 0) {
+                phenotypes = phenotypes.concat(this.props.prio.terms[i].term.name + ' (' + this.props.prio.terms[i].term.id + ')')
+            } else if (i !== this.props.prio.terms.length - 1) {
+                phenotypes = phenotypes.concat(', ' + this.props.prio.terms[i].term.name + ' (' + this.props.prio.terms[i].term.id + ')')
+            } else {
+                phenotypes = phenotypes.concat(' and ' + this.props.prio.terms[i].term.name + ' (' + this.props.prio.terms[i].term.id + ')')
+            }
+        }
+        var phenotypesDiv = <p>{phenotypes + "."}</p>
+        return (phenotypesDiv)*/}
+        
+    }
+})
+
+
+
+
+
+
+
+var ShowPhenotypes3 = React.createClass({
+
+
+    render: function() {
+
+        var hoverZScores = this.props.hoverItem
+
+
+        var rows = []
+        for (var i = 0; i < this.props.prio.terms.length; i++) {
+
+
+            var zToCol = hoverZScores ? Math.ceil(hoverZScores[i] * 60) : rowtype
+
+            console.log(zToCol)
+
+            var hoverColour =  hoverZScores ? 'rgb(' + zToCol + ',255,255)' : rowtype
+
+            console.log(i + ': ' + hoverColour)
+            var rowtype = i % 2 === 0 ? 'datarow evenrow' : 'datarow oddrow'
+            var backgroundColour = hoverZScores ? hoverColour : rowtype
+
+            backgroundColour = hoverZScores ? getRGB(hoverZScores[i]) : 'rgb(255,255,0)'
+
+            var square =
+            <div style={this.props.style} title={this.props.prio.results[i].gene.biotype}>
+                <svg viewBox='0 0 10 10' width={20} height={this.props.h}>
+                <rect x1='0' y1='0' width='10' height='10' style={{fill: backgroundColour}} />
+                </svg>
+                </div>
+
+
+
+            rows.push(
+                <tr className = {rowtype} >
+                <td>{square}</td>
+                <td style={{textAlign: 'left'}}>{this.props.prio.terms[i].term.name}</td>
+                <td style={{textAlign: 'center'}}>{this.props.prio.terms[i].term.numAnnotatedGenes}</td>
+                <td style={{textAlign: 'center'}}><a style={{color: color.colors.gnblack}} href={this.props.prio.terms[i].term.url} target="_blank">{this.props.prio.terms[i].term.id}</a></td>
+                <td style={{textAlign: 'center'}}>{hoverZScores ? hoverZScores[i] : ""}</td>
+                </tr>
+                )
+        }
+
+        return (
+            <table className='gn-gene-table datatable' style={{width: '70%'}}>
+            <tbody>
+            <tr>
+            <th></th>
+            <th style={{textAlign: 'left'}}>PHENOTYPE</th>
+            <th style={{textAlign: 'center'}}>ANNOTATED GENES</th>
+            <th style={{textAlign: 'center'}}>HPO-TERM</th>
+            <th>Zscore</th>
+            
+            </tr>
+            {rows}
+            </tbody>
+            </table>
+        )
+        
+    }
+})
+
+
+
+
+
+
+
+
 var Table = React.createClass({
 
     render: function() {
@@ -71,7 +277,7 @@ var Table = React.createClass({
         for (var i = 0; i < this.props.prio.results.length; i++) {
             var rowtype = i % 2 === 0 ? 'datarow evenrow' : 'datarow oddrow'
 
-        {/* create networklink: */}
+        {/* networklink: */}
             var phens = ""
             for (var j = 0; j < this.props.prio.terms.length; j++) {
                 phens = phens.concat(this.props.prio.terms[j].term.id + ',')
@@ -80,15 +286,15 @@ var Table = React.createClass({
             var networkLink = GN.urls.networkPage + phens + gene
 
             var square =
-
-            <div style={this.props.style} title={this.props.prio.results[i].gene.biotype}>
+                <div style={this.props.style} title={this.props.prio.results[i].gene.biotype}>
                 <svg viewBox='0 0 10 10' width={20} height={this.props.h}>
                 <rect x1='0' y1='0' width='10' height='10' style={{fill: color.biotype2color[this.props.prio.results[i].gene.biotype] || color.colors.default}} />
                 </svg>
                 </div>
 
+
             rows.push(
-                <tr className = {rowtype} onMouseOver={this.props.onMouseOver.bind(null, this.props.prio.results[i].gene)}>
+                <tr className = {rowtype} onMouseOver={this.props.onMouseOver.bind(null, this.props.prio.results[i].predicted)}>
                 
                 <td>{square}</td>
                 <td>{this.props.prio.results[i].gene.name}</td>
@@ -102,7 +308,7 @@ var Table = React.createClass({
                 
                 <td style={{textAlign: 'center'}} 
 
-                title={this.props.isAnnotated ? this.props.prio.results[i].annotated : "Not annotated to any of the phenotypes."}>
+                title={this.props.prio.results[i].annotated.length == 0 ? "Not annotated to any of the phenotypes." : this.props.prio.results[i].annotated}>
                     {this.props.prio.results[i].annotated.length == 0 ? <SVGCollection.NotAnnotated /> : <SVGCollection.Annotated />}
                     {/*this.props.prio.results[i].annotated[0] == -1 ? <SVGCollection.Annotated /> : <SVGCollection.NotAnnotated /> */}
                 </td>
@@ -139,9 +345,9 @@ var Diagnosis = React.createClass({
         }
     },
 
-    handleMouseOver: function(thing) {
+    handleMouseOver: function(hoverGene) {
         this.setState({
-            hoverItem: thing
+            hoverItem: hoverGene
         })
     },
     
@@ -182,6 +388,7 @@ var Diagnosis = React.createClass({
     render: function() {
 
         console.log(this.state.data)
+        console.log(this.state.hoverItem)
         
         if (!this.state.data) {
             return null
@@ -190,17 +397,25 @@ var Diagnosis = React.createClass({
         return (
 		<DocumentTitle title={'Diagnosis' + GN.pageTitleSuffix}>
                 <div>
+                <p style={{width: '70%'}}>A few ideas for the phenotypes table. (I like the 3rd the best, seeing as the other two are either kind of ugly or illegible for the higher Z-scores, but the squares might be a bit too understated...?) Also, I first put the Zscore column in there to check the colours, but it might be useful to keep?</p>
+              
+
                 {/*<div>
                 {this.state.message}
             </div>*/}
                 <div>
                 {/*<p>{this.state.data ? 'Showing results for: ' + this.state.data.terms[0].term.name + ' (' + this.state.data.terms[0].term.id + ')' : 'loading'}</p>*/}
 
-                <p>{this.state.data ? <ShowPhenotypes prio={this.state.data} /> : 'loading'}</p>
+                <p>{this.state.data ? <ShowPhenotypes prio={this.state.data} hoverItem={this.state.hoverItem} /> : 'loading'}</p>
 
             </div>
+
+                <div><p>{this.state.data ? <ShowPhenotypes2 prio={this.state.data} hoverItem={this.state.hoverItem} /> : 'loading'}</p></div>
+                <div><p>{this.state.data ? <ShowPhenotypes3 prio={this.state.data} hoverItem={this.state.hoverItem} /> : 'loading'}</p></div>
+
+
                 <div>
-                <p>{this.state.data ? this.state.data.results.length + ' results for these phenotypes:' : 'loading'}</p>
+                <p>{this.state.data ? this.state.data.results.length + ' results for the combination of these ' + this.state.data.terms.length + ' phenotypes:' : 'loading'}</p>
             </div>
                 {/*<div>
                 {this.state.data ? 'top gene: ' + this.state.data.results[0].gene.name : null}
