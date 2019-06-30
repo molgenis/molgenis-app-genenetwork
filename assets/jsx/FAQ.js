26c278b633e7a4cdc1aa1b0bcc22e49657d1aff6
Roy Oelen <r.oelen@umcg.nl>
2019-06-30 20:55:02 +0200

    - added text and link in FAQ to refer to CLI GADO

--- assets/jsx/FAQ.js
+++ assets/jsx/FAQ.js
@@ -12,6 +12,7 @@ var How = React.createClass({
                     <ul>
                         <li><a href={"#what-is-genenetwork"}>What is GeneNetwork?</a></li>
                         <li><a href={"#what-is-gado"}>What is GADO?</a></li>
+                        <li><a href={"#can-i-run-gado-locally"}>Can I run GADO locally?</a></li>
                         <li><a href={"#why-cant-my-term-be-used"}>Why can’t my term be used?</a></li>
                         <li><a href={"#why-is-my-term-not-found"}>Why is my term not found?</a></li>
                         <li><a href={"#gene-not-found"}>My favorite candidate gene for patient is not found back in the top of the results?</a></li>
@@ -36,6 +37,12 @@ var How = React.createClass({
                         the discovery of new disease-causing genes.
                     </p>
                     <img title='GeneNetwork' style={{width: '800px'}} src={GN.urls.main + '/images/gado.png'} />
+
+                    <h3 id="can-i-run-gado-locally">Can I run GADO locally?</h3>
+                    <p>
+                        We have created a standalone version of GADO that can easily be integrated in an automated analysis pipeline.
+                        For more information see: <a href="https://github.com/molgenis/systemsgenetics/wiki/GADO-Command-line">GADO Command Line</a>
+                    </p>
 
                     <h3 id="why-cant-my-term-be-used">Why can’t my term be used?</h3>
                     <p>
