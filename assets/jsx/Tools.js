38fe5cf15d512a79345e666b598d1e05ca392191
npklein <niekdeklein@gmail.com>
2019-10-08 10:25:53 +0200

    change logos,name etc

--- assets/jsx/Tools.js
+++ assets/jsx/Tools.js
@@ -4,16 +4,20 @@ var BoxFunctionEnrichment = require('./BoxFunctionEnrichment');
 var GN = require("../../config/gn");
 var color = require('../js/color');
 
+/*
+Removed because not working:
+<Box
+                    title="GADO: GeneNetwork Assisted Diagnostic Optimization"
+                    text="Prioritize genes based on one or multiple HPO phenotypes."
+                    url={GN.urls.diagnosisPage} />
+ */
 var Tools = React.createClass({
 
     render: function() {
         return (
             <div style={{backgroundColor: color.colors.gnwhite, marginTop: '10px', padding: '20px', flex: '1'}}>
                 <h2 style={{display: 'inline'}}>TOOLS</h2>
-                <Box
-                    title="GADO: GeneNetwork Assisted Diagnostic Optimization"
-                    text="Prioritize genes based on one or multiple HPO phenotypes."
-                    url={GN.urls.diagnosisPage} />
+
                 <BoxFunctionEnrichment
                     title="Function enrichment"
                     text="Predict which pathways are enriched for a set of genes."
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,20 @@
 # Stop immediately if any of these scripts fail
 set -e
 
-echo "START ENTYPOINT!"
+echo "Start entrypoint script"
+echo "Create elastic index"
 # Run the migration/seed jobs
 node data_scripts/createElasticIndex.js
+echo "Done"
+echo "Parse genes to elastic"
 node data_scripts/parseGenesToElastic.js
+echo "Done"
 # add the HPO terms
+echo "Parse HPO to elastic"
 node data_scripts/parseHpoOboToElastic.js
+echo "Done"
 
 # Run the CMD / `docker run ...` command
 exec "$@"
+
+echo "Finished with entrypoint script"
