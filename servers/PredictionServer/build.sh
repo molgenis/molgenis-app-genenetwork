c1d322718c50c76ccf4526128b14ed744c323b7d
npklein <niekdeklein@gmail.com>
2019-10-08 11:03:39 +0200

    testing worked, updating version to server

--- servers/PredictionServer/build.sh
+++ servers/PredictionServer/build.sh
@@ -1,2 +1,2 @@
-docker build -t ndeklein/predictionserver:v1.2.test .
-docker push ndeklein/predictionserver:v1.2.test
+docker build -t ndeklein/predictionserver:v1.2.server .
+docker push ndeklein/predictionserver:v1.2.server
