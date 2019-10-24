3d65c69a3e48217390e2c493127d83e8c9754965
npklein <niekdeklein@gmail.com>
2019-10-24 15:19:16 -0400

    add metabrain qtl link

--- assets/jsx/GeneNetwork.js
+++ assets/jsx/GeneNetwork.js
@@ -29,6 +29,9 @@ var history = createBrowserHistory();
 ReactDOM.render(<Router history={history}>
                     <Route>
                         <Route path='/' component = {Landing}>
+                            <Route path='/qtl'>
+                                MetaBrain QTLs
+                            </Route>
                             <Route path='/faq' component = {FAQ} />
                             <Route path='/about' component = {About} />
                             <Route path='/api' component = {API} />
