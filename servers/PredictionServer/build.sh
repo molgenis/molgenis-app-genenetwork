255fbbb5a9f17fba8ec66912610d657d9fe99c94
npklein <niekdeklein@gmail.com>
2020-03-31 14:06:31 +0200

    server version 1 up

--- servers/PredictionServer/build.sh
+++ servers/PredictionServer/build.sh
@@ -1,2 +1,2 @@
-docker build -t ndeklein/predictionserver:v1.2.server .
-docker push ndeklein/predictionserver:v1.2.server
+docker build -t ndeklein/predictionserver:v1.3.server .
+docker push ndeklein/predictionserver:v1.3.server
