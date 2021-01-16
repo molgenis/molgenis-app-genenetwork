c1ce97b6af3ad2967e486c0617cc2d6871a7dff1
Roy Oelen <r.oelen@umcg.nl>
2021-01-16 17:13:39 +0100

    - updated some URLs to see if that does anything

--- config/version.js
+++ config/version.js
@@ -2,13 +2,13 @@ module.exports.version = {
     serviceName: 'Gene Network',
     version: '2.0',
     affiliation: 'Department of Genetics, University Medical Center Groningen',
-    mainUrl: 'http://www.genenetwork.nl',
-    coregNetworkUrl: 'http://www.genenetwork.nl/#/network/',
-    apiUrl: 'http://www.genenetwork.nl/api/v1',
+    mainUrl: 'https://kidney-network.gcc.rug.nl/',
+    coregNetworkUrl: 'https://kidney-network.gcc.rug.nl//#/network/',
+    apiUrl: 'https://kidney-network.gcc.rug.nl/api/v1',
     comment: function() {
         // return '//' + this.serviceName + ' ' + this.version + ' // ' + this.affiliation + ' // ' + this.mainUrl
         return this.serviceName + ' ' + this.version + ' / ' + this.affiliation + ' / ' + this.mainUrl
     },
-    biomartRelease: "V83",
+    biomartRelease: "V98",
     assemblyRelease: "GRCh38"
 }
