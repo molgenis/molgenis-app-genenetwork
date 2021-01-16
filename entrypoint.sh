479a0e664a933005e5f5e8f907f4304311fe3571
Roy Oelen <r.oelen@umcg.nl>
2021-01-16 19:36:22 +0100

    - trying to parse HPO terms to elastic in main view

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# author npklein
+# author npklein,royoelen
 
 # Stop immediately if any of these scripts fail
 set -e
@@ -11,6 +11,8 @@ node data_scripts/createElasticIndex.js
 node data_scripts/parseGenesToElastic.js
 # add the HPO terms
 node data_scripts/parseHpoOboToElastic.js
+# add the HPO terms to the elastic search in main view
+node data_scripts/parseTermsToElastic.js
 
 # Run the CMD / `docker run ...` command
 exec "$@"
