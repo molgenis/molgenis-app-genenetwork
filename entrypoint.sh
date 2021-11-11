1f69351101f5a40fed38fe7a0534b2e609063912
Roy Oelen <51436605+royoelen@users.noreply.github.com>
2021-11-11 16:00:35 +0100

    fixed missing datascript for elasticsearch
    fixed missing datascript for elasticsearch

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,8 @@ node data_scripts/createElasticIndex.js
 node data_scripts/parseGenesToElastic.js
 # add the HPO terms
 node data_scripts/parseHpoOboToElastic.js
+# add the HPO terms to the elastic search in main view
+node data_scripts/parseTermsToElastic.js
 
 # Run the CMD / `docker run ...` command
 exec "$@"
