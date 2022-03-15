b4477aedd666c69046845da6e02e031802e51c2d
Harm-Jan Westra <westra.harmjan@outlook.com>
2022-03-15 10:13:38 +0100

    older docker-compose does not readily support hostname based references; revert to container-name based references in docker-compose.yml

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 npm run build
 version=v1.16.server
 docker build -t ndeklein/metabrainnetwork:$version .
-#docker push ndeklein/metabrainnetwork:$version
+docker push ndeklein/metabrainnetwork:$version
