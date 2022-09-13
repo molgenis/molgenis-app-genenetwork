5a2e5a44329457834fdf90dcde8488abe44092f4
Harm-Jan Westra <westra.harmjan@outlook.com>
2022-09-13 14:08:01 +0200

    Fix GPO labels, add wait-for-it to entrypoint

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,9 @@ FROM node:12.22.12
 # Set the working directory to /app
 WORKDIR /app
 
-RUN apt-get update && apt-get install -y librsvg2-bin; set -eux; apt-get install curl;
-
+RUN apt-get update && apt-get install -y librsvg2-bin; set -eux; apt-get install -y curl;
+RUN apt-get install -y apt-utils
+RUN apt-get install -y wait-for-it
 
 # Copy the current directory contents into the container at /app
 COPY package*.json entrypoint.sh ./
--- populate_database.sh
+++ populate_database.sh
@@ -6,66 +6,89 @@ go_P_n=150
 kegg_n=500
 reactome_n=700
 hpo_n=1000
+
+
+outdir=/mnt/docker/metabrainnetwork/data/
+datadir=/mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/
+datadirB=/mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/
 ### 2020-03-23-goa_human_C ###
-mkdir -p /mnt/d/metabrainnetwork/data/
+# mkdir -p /mnt/d/metabrainnetwork/data/
+
+ python3 data_scripts/fixGODescriptions.py \
+	$datadir/2022-09-12-goToDesc.txt.gz \
+	$datadir/$go_C_n/2020-03-23-goa_human_C.${go_C_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat.txt.gz \
+	$datadir/$go_C_n/2020-03-23-goa_human_C.${go_C_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat-fix.txt.gz
+
  bash data_scripts/populate_geneset.sh \
-   -d /mnt/d/metabrainnetwork/data/ \
+   -d $outdir \
    -b GO_C \
-   -t /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$go_C_n/2020-03-23-goa_human_C.${go_C_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat.txt.gz \
-   -i /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$go_C_n/2020-03-23-goa_human_C.matrix_gnInputFormat.txt.gz \
-   -z /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$go_C_n/2020-03-23-goa_human_C.${go_C_n}_eigenvectors.predictions.bonSigOnly_termNames_tranposed.txt.gz \
-   -a /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$go_C_n/2020-03-23-goa_human_C.${go_C_n}_eigenvectors.predictions.AUC.bonferonni_gnInputFormat.txt.gz
+   -t $datadir/$go_C_n/2020-03-23-goa_human_C.${go_C_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat-fix.txt.gz \
+   -i $datadir/$go_C_n/2020-03-23-goa_human_C.matrix_gnInputFormat.txt.gz \
+   -z $datadir/$go_C_n/2020-03-23-goa_human_C.${go_C_n}_eigenvectors.predictions.bonSigOnly_termNames_tranposed.txt.gz \
+   -a $datadir/$go_C_n/2020-03-23-goa_human_C.${go_C_n}_eigenvectors.predictions.AUC.bonferonni_gnInputFormat.txt.gz
+
 # ### 2020-03-23-goa_human_F ###
+ python3 data_scripts/fixGODescriptions.py \
+	$datadir/2022-09-12-goToDesc.txt.gz \
+	$datadir/$go_F_n/2020-03-23-goa_human_F.${go_F_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat.txt.gz \
+	$datadir/$go_F_n/2020-03-23-goa_human_F.${go_F_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat-fix.txt.gz
+
  bash data_scripts/populate_geneset.sh \
-   -d /mnt/d/metabrainnetwork/data/ \
+   -d $outdir \
    -b GO_F \
-   -t /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$go_F_n/2020-03-23-goa_human_F.${go_F_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat.txt.gz \
-   -i /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$go_F_n/2020-03-23-goa_human_F.matrix_gnInputFormat.txt.gz \
-   -z /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$go_F_n/2020-03-23-goa_human_F.${go_F_n}_eigenvectors.predictions.bonSigOnly_termNames_tranposed.txt.gz \
-   -a /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$go_F_n/2020-03-23-goa_human_F.${go_F_n}_eigenvectors.predictions.AUC.bonferonni_gnInputFormat.txt.gz
+   -t $datadir/$go_F_n/2020-03-23-goa_human_F.${go_F_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat-fix.txt.gz \
+   -i $datadir/$go_F_n/2020-03-23-goa_human_F.matrix_gnInputFormat.txt.gz \
+   -z $datadir/$go_F_n/2020-03-23-goa_human_F.${go_F_n}_eigenvectors.predictions.bonSigOnly_termNames_tranposed.txt.gz \
+   -a $datadir/$go_F_n/2020-03-23-goa_human_F.${go_F_n}_eigenvectors.predictions.AUC.bonferonni_gnInputFormat.txt.gz
   
 # ### 2020-03-23-goa_human_P ###
- bash data_scripts/populate_geneset.sh \
-   -d /mnt/d/metabrainnetwork/data/ \
-   -b GO_P \
-   -t /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$go_P_n/2020-03-23-goa_human_P.${go_P_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat.txt.gz \
-   -i /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$go_P_n/2020-03-23-goa_human_P.matrix_gnInputFormat.txt.gz \
-   -z /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$go_P_n/2020-03-23-goa_human_P.${go_P_n}_eigenvectors.predictions.bonSigOnly_termNames_tranposed.txt.gz \
-   -a /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$go_P_n/2020-03-23-goa_human_P.${go_P_n}_eigenvectors.predictions.AUC.bonferonni_gnInputFormat.txt.gz
+ python3 data_scripts/fixGODescriptions.py \
+	$datadir/2022-09-12-goToDesc.txt.gz \
+	$datadir/$go_P_n/2020-03-23-goa_human_P.${go_P_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat.txt.gz \
+	$datadir/$go_P_n/2020-03-23-goa_human_P.${go_P_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat-fix.txt.gz
 
+ bash data_scripts/populate_geneset.sh \
+   -d $outdir \
+   -b GO_P \
+   -t $datadir/$go_P_n/2020-03-23-goa_human_P.${go_P_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat-fix.txt.gz \
+   -i $datadir/$go_P_n/2020-03-23-goa_human_P.matrix_gnInputFormat.txt.gz \
+   -z $datadir/$go_P_n/2020-03-23-goa_human_P.${go_P_n}_eigenvectors.predictions.bonSigOnly_termNames_tranposed.txt.gz \
+   -a $datadir/$go_P_n/2020-03-23-goa_human_P.${go_P_n}_eigenvectors.predictions.AUC.bonferonni_gnInputFormat.txt.gz
+
+# exit 0
 
 # ### 2020-03-28-c2.cp.kegg.v7.0 ###
  bash data_scripts/populate_geneset.sh \
-   -d /mnt/d/metabrainnetwork/data/ \
+   -d $outdir \
    -b KEGG \
-   -t /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$kegg_n/2020-03-28-c2.cp.kegg.v7.0.${kegg_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat.txt.gz \
-   -i /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$kegg_n/2020-03-28-c2.cp.kegg.v7.0.matrix_gnInputFormat.txt.gz \
-   -z /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$kegg_n/2020-03-28-c2.cp.kegg.v7.0.${kegg_n}_eigenvectors.predictions.bonSigOnly_termNames_tranposed.txt.gz \
-   -a /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$kegg_n/2020-03-28-c2.cp.kegg.v7.0.${kegg_n}_eigenvectors.predictions.AUC.bonferonni_gnInputFormat.txt.gz
+   -t $datadir/$kegg_n/2020-03-28-c2.cp.kegg.v7.0.${kegg_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat.txt.gz \
+   -i $datadir/$kegg_n/2020-03-28-c2.cp.kegg.v7.0.matrix_gnInputFormat.txt.gz \
+   -z $datadir/$kegg_n/2020-03-28-c2.cp.kegg.v7.0.${kegg_n}_eigenvectors.predictions.bonSigOnly_termNames_tranposed.txt.gz \
+   -a $datadir/$kegg_n/2020-03-28-c2.cp.kegg.v7.0.${kegg_n}_eigenvectors.predictions.AUC.bonferonni_gnInputFormat.txt.gz
 
  bash data_scripts/populate_geneset.sh \
-   -d /mnt/d/metabrainnetwork/data/ \
+   -d $outdir \
    -b REACTOME \
-   -t /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$reactome_n/2020-03-28-Ensembl2Reactome_All_Levels.${reactome_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat.txt.gz \
-   -i /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$reactome_n/2020-03-28-Ensembl2Reactome_All_Levels.matrix_gnInputFormat.txt.gz \
-   -z /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$reactome_n//2020-03-28-Ensembl2Reactome_All_Levels.${reactome_n}_eigenvectors.predictions.bonSigOnly_termNames_tranposed.txt.gz \
-   -a /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$reactome_n//2020-03-28-Ensembl2Reactome_All_Levels.${reactome_n}_eigenvectors.predictions.AUC.bonferonni_gnInputFormat.txt.gz
+   -t $datadir/$reactome_n/2020-03-28-Ensembl2Reactome_All_Levels.${reactome_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat.txt.gz \
+   -i $datadir/$reactome_n/2020-03-28-Ensembl2Reactome_All_Levels.matrix_gnInputFormat.txt.gz \
+   -z $datadir/$reactome_n//2020-03-28-Ensembl2Reactome_All_Levels.${reactome_n}_eigenvectors.predictions.bonSigOnly_termNames_tranposed.txt.gz \
+   -a $datadir/$reactome_n//2020-03-28-Ensembl2Reactome_All_Levels.${reactome_n}_eigenvectors.predictions.AUC.bonferonni_gnInputFormat.txt.gz
 
 ### 2020-03-28-HPO-phenotype-to-genes ###
  bash data_scripts/populate_geneset.sh \
-   -d /mnt/d/metabrainnetwork/data/ \
+   -d $outdir \
    -b HPO \
-   -t /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$hpo_n/2020-03-28-HPO-phenotype-to-genes.${hpo_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat.txt.gz \
-   -i /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$hpo_n/2020-03-28-HPO-phenotype-to-genes.matrix_gnInputFormat.txt.gz \
-   -z /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$hpo_n/2020-03-28-HPO-phenotype-to-genes.${hpo_n}_eigenvectors.predictions.bonSigOnly_termNames_tranposed.txt.gz \
-   -a /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/11_ImportToWebsite/$hpo_n/2020-03-28-HPO-phenotype-to-genes.${hpo_n}_eigenvectors.predictions.AUC.bonferonni_gnInputFormat.txt.gz
+   -t $datadir/$hpo_n/2020-03-28-HPO-phenotype-to-genes.${hpo_n}_eigenvectors.predictions.AUC.bonSigTerms_gnInputFormat.txt.gz \
+   -i $datadir/$hpo_n/2020-03-28-HPO-phenotype-to-genes.matrix_gnInputFormat.txt.gz \
+   -z $datadir/$hpo_n/2020-03-28-HPO-phenotype-to-genes.${hpo_n}_eigenvectors.predictions.bonSigOnly_termNames_tranposed.txt.gz \
+   -a $datadir/$hpo_n/2020-03-28-HPO-phenotype-to-genes.${hpo_n}_eigenvectors.predictions.AUC.bonferonni_gnInputFormat.txt.gz
 
 
 bash data_scripts/populate_geneDB_and_coregulation.sh \
-  -d /mnt/d/metabrainnetwork/data/ \
-  -g /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/2020-03-01.all-cohorts-kallisto-genes.txt.gz \
-  -e /mnt/d/metabrainnetwork/data/files/ENSGToGeneNameHGNCBiotypeChromosomeStartStopStrandAndDescriptionV98.filteredOnMetabrainGenes.txt.gz \
-  -c /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/9_eigenvector_correlation_matrix/MetaBrain.1000_eigenvectors.columnsRowsCenterScaled.correlation.txt.gz \
+  -d $outdir \
+  -g $datadirB/2020-03-01.all-cohorts-kallisto-genes.txt.gz \
+  -e $outdir/files/ENSGToGeneNameHGNCBiotypeChromosomeStartStopStrandAndDescriptionV98.filteredOnMetabrainGenes.txt.gz \
+  -c $datadirB/9_eigenvector_correlation_matrix/MetaBrain.1000_eigenvectors.columnsRowsCenterScaled.correlation.txt.gz \
   -n 1000
   
 #  -e /mnt/y/metabrainnetwork/2020-02-28-MetaBrainNetwork/output/2020-03-01.all-cohorts-kallisto-genes-tohgnc.txt.gz \
